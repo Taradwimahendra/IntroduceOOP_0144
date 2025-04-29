902f90ea46d902a7f47dd3a940c8f435b66c6523
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-04-29 18:46:26 +0700

    membuat procedure printdata

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+int nim;
+    string nama;
+    float nilai;
+    
+    void printData(){
+        cout << "Nim : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
