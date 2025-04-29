9e585e7d784e39c5da478638e8c048c60e899a5e
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-04-29 18:47:24 +0700

    membuat object mahasiswa

--- oop.cpp
+++ oop.cpp
@@ -6,5 +6,13 @@ class Mahasiswa{
     int nim;
     string nama;
     float nilai;
-
+    
+    void printData(){
+        cout << "Nim : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
 }; 
+int main(){
+    Mahasiswa mhs; 
+}
