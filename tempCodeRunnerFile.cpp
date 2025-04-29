1130d0a17fb070410c463911edd9483c4a947e6c
Taradwimahendra <taradwimahendra15@gmail.com>
2025-04-29 18:55:12 +0700

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-int nim;
-    string nama;
-    float nilai;
-    
-    void printData(){
-        cout << "Nim : " << nim << endl;
-        cout << "Nama : " << nama << endl;
-        cout << "Nilai : " << nilai << endl;
-    }
